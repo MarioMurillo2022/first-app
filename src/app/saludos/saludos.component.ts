bb228266d8297fa96461ce77ab9c92a481c78d42
MarioMurillo2022 <Mariomun17@gmail.com>
2024-05-26 01:15:26 -0600

    componente saludos
    se anadio el componente saludos con el cli ng generate component saludos

---
+++ src/app/saludos/saludos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saludos',
+  standalone: true,
+  imports: [],
+  templateUrl: './saludos.component.html',
+  styleUrl: './saludos.component.css'
+})
+export class SaludosComponent {
+
+}

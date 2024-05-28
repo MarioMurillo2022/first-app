c8defc3bc3eb708d18c5fe6ef07f41090ff2584a
MarioMurillo2022 <Mariomun17@gmail.com>
2024-05-27 22:36:28 -0600

    rutas
    se importaron los componentes nuevos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SaludosComponent } from './saludos/saludos.component';
+import { GaleriaComponent } from './galeria/galeria.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, SaludosComponent, GaleriaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
